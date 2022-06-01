9b83c1c301bd79620c7e5f47b260f8e5fad15f9f
betechnoid <bhshrivastav@gmail.com>
2022-06-01 23:21:04 +0530

    update

---
+++ src/logger/logger.js
@@ -0,0 +1,7 @@
+function welcome() {
+    const name="abhishek"
+    return ("Welcome to my application. I am "+name+" and a part of Functionup Radon Cohort")
+    
+}
+welcome()
+module.exports.welcome =welcome()
