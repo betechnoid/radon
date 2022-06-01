b8363b74e566374e3b3e708d758990a56e0e3688
betechnoid <bhshrivastav@gmail.com>
2022-06-01 23:21:52 +0530

    done

---
+++ src/util/helper.js
@@ -0,0 +1,11 @@
+
+let days="Date 1";
+let months="Months May";
+
+function topic(){
+    return 'Radon, W2 D3, the topic for today is Nodejs module system'
+}
+topic()
+module.exports.days=days;
+module.exports.months=months;
+module.exports.topic=topic();
---
+++ src/validator/formatter.js
@@ -0,0 +1,11 @@
+let text="    this is trim    "
+let trimming=text.trim();
+
+let upper="this is Upper Case"
+let rupper=upper.toUpperCase();
+
+let lower=upper.toLowerCase();
+
+module.exports.trimming=trimming;
+module.exports.rupper=rupper;
+module.exports.lower=lower;
